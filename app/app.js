a94e2e15eb13eab43a6f40a0e1dc60cd4a30437a
omedale <omedale@gmail.com>
2017-05-09 06:53:29 +0100

    new commit

--- app/app.js
+++ app/app.js
@@ -1,16 +1,23 @@
-const getPrimes =(n)=>{
-	let generatedPrime =[];
-	let counter = 1;
-	 for(let count=1; count<=n; count++){
-	 		for(let county=1; county<= count; county++){
-	 		if ((count%county)==0){
+module.exports = {
+
+
+
+	 getPrimes: (n)=>{
+	 	let generatedPrime =[];
+	 for(let count=0; count<=n; count++){
+	 	   let counter = 0;
+	 		for(let county=1; county <= count; county++){
+	 		if (count%county == 0){
 	 			counter +=1;
 	 		}
-	 		if(counter==2){
-	 			generatedPrime=count;
-	 		}
-	   }		
+	 		
+	   }
+	   
+	   	if(counter==2){
+	 			generatedPrime.push(count);
+	 		}	
 	 }
+	return generatedPrime.toString();
+}
 
-	return generatedPrime;
 }
--- spec/primeSpec.js
+++ spec/primeSpec.js
@@ -1,8 +1,10 @@
-
  const myApp = require("../app/app.js");
+
 describe("A suite is just a function", function() {
-  it("expect return 8 for Celcius value 2 , 2", function() {
-    expect(myApp.aa() == 1);
-  })
+		  it("to expect return prime numbers in 10", function() {
+		  	let list = [ 2, 3, 5, 7];
+		  	expect(myApp.getPrimes(10)).toBe(list.toString());
+		   // expect(myApp.getPrimes(10) == [2,3,5,7]);
+		  })
 });
-    
+
